0af1776fb5d4d20131e9523f2d287179a4fa4f54
kimuson <kimuson@jxpress.net>
2022-03-12 21:01:05 +0900

    test: add test case

--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -1,7 +1,43 @@
 package a
 
+import (
+	"fmt"
+	"log"
+)
+
 func f() {
 	// The pattern can be written in regular expression.
-	var gopher int // want "pattern"
-	print(gopher)  // want "identifier is gopher"
+
+	err := hoge()
+	if err != nil {
+		log.Println(err)
+	}
+
+	hoge() // want "NG"
+
+	_ = hoge() //want "NG"
+
+	log.Println(hoge())
+
+	i, err := hoge2()
+	if err != nil {
+		log.Println(i, err)
+	}
+
+	hoge2() // want "NG"
+
+	log.Println(hoge2())
+
+	fmt.Println("Hello")
+
+	i2, _ := hoge2() // want "NG"
+	log.Println(i2)
+}
+
+func hoge() error {
+	return nil
+}
+
+func hoge2() (int, error) {
+	return 0, nil
 }
